64379c41c8d4db6fd1d1d56e97130c7da24135e3
wangjunyang <775296271@qq.com>
2025-01-25 22:29:21 +0800

    feat：应用管理组件树开发

--- src/app/modules/home/page/app-manage/app-manage.component.ts
+++ src/app/modules/home/page/app-manage/app-manage.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { HsTreeComponent } from "../../../../shared/components/hs-tree/hs-tree.component";
 
 @Component({
   selector: 'app-app-manage',
   templateUrl: './app-manage.component.html',
+  imports: [MatDividerModule, HsTreeComponent]
 })
 export class AppManageComponent implements OnInit {
 
---
+++ src/app/shared/components/hs-tree/hs-tree.component.ts
@@ -0,0 +1,110 @@
+import {Component} from '@angular/core';
+import {ArrayDataSource} from '@angular/cdk/collections';
+import {FlatTreeControl, CdkTreeModule} from '@angular/cdk/tree';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+const TREE_DATA: ExampleFlatNode[] = [
+  {
+    name: 'Fruit',
+    expandable: true,
+    level: 0,
+  },
+  {
+    name: 'Apple',
+    expandable: false,
+    level: 1,
+  },
+  {
+    name: 'Banana',
+    expandable: false,
+    level: 1,
+  },
+  {
+    name: 'Fruit loops',
+    expandable: false,
+    level: 1,
+  },
+  {
+    name: 'Vegetables',
+    expandable: true,
+    level: 0,
+  },
+  {
+    name: 'Green',
+    expandable: true,
+    level: 1,
+  },
+  {
+    name: 'Broccoli',
+    expandable: false,
+    level: 2,
+  },
+  {
+    name: 'Brussels sprouts',
+    expandable: false,
+    level: 2,
+  },
+  {
+    name: 'Orange',
+    expandable: true,
+    level: 1,
+  },
+  {
+    name: 'Pumpkins',
+    expandable: false,
+    level: 2,
+  },
+  {
+    name: 'Carrots',
+    expandable: false,
+    level: 2,
+  },
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+  isExpanded?: boolean;
+}
+@Component({
+  selector: 'hs-tree',
+  styleUrl: './hs-tree.component.less',
+  templateUrl: './hs-tree.component.html',
+  imports: [CdkTreeModule, MatButtonModule, MatIconModule],
+})
+export class HsTreeComponent {
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  dataSource = new ArrayDataSource(TREE_DATA);
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+  getParentNode(node: ExampleFlatNode) {
+    const nodeIndex = TREE_DATA.indexOf(node);
+
+    for (let i = nodeIndex - 1; i >= 0; i--) {
+      if (TREE_DATA[i].level === node.level - 1) {
+        return TREE_DATA[i];
+      }
+    }
+
+    return null;
+  }
+
+  shouldRender(node: ExampleFlatNode) {
+    let parent = this.getParentNode(node);
+    while (parent) {
+      if (!parent.isExpanded) {
+        return false;
+      }
+      parent = this.getParentNode(parent);
+    }
+    return true;
+  }
+}
