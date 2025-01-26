db902001037b363235d15f8e188df04ac999bc37
wangjunyang <775296271@qq.com>
2025-01-26 11:48:18 +0800

    feat:加入material icons依赖包，用到目录树中

--- src/app/modules/home/page/app-manage/app-manage.component.ts
+++ src/app/modules/home/page/app-manage/app-manage.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { HsTreeComponent } from "../../../../shared/components/hs-tree/hs-tree.component";
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-app-manage',
   templateUrl: './app-manage.component.html',
-  imports: [MatDividerModule, HsTreeComponent]
+  imports: [
+    MatDividerModule,
+    HsTreeComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ]
 })
 export class AppManageComponent implements OnInit {
+  fileName = new FormControl("");
 
   constructor() { }
 
