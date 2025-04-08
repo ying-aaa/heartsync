4b0e32c154063584b533ae898631da9d626ae74f
Qiuy <775296271@qq.com>
2025-04-08 22:15:14 +0800

    ui-delete

--- ui-ngx/src/app/modules/components/widget-code/dynamic-widget.component.ts
+++ ui-ngx/src/app/modules/components/widget-code/dynamic-widget.component.ts
@@ -1,20 +1,3 @@
-///
-/// Copyright © 2016-2025 The Thingsboard Authors
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///     http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
-
-
 import { Directive, inject, Injector, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
@@ -24,18 +7,7 @@ import { widgetContextToken } from '@src/app/shared/models/widget-component.mode
 
 @Directive()
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
-export class DynamicWidgetComponent  implements OnInit, OnDestroy {
-
-  executingRpcRequest: boolean;
-  rpcEnabled: boolean;
-  rpcErrorText: string;
-  rpcRejection: HttpErrorResponse | Error;
-
-  [key: string]: any;
-
-  validators = Validators;
-
-  public fb = inject(UntypedFormBuilder);
+export class DynamicWidgetComponent implements OnInit, OnDestroy {
   public readonly $injector = inject(Injector);
   public readonly ctx = {} as any;
 
@@ -48,11 +20,7 @@ export class DynamicWidgetComponent  implements OnInit, OnDestroy {
     this.ctx.$scope = this;
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  ngOnDestroy(): void {
-
-  }
+  ngOnDestroy(): void {}
 }
