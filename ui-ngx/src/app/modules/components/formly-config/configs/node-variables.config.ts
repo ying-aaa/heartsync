a9cd67d206bb128fb1f4341277fbb1aced8e4c15
wangjunyang <775296271@qq.com>
2025-11-22 16:14:07 +0800

    stash：节点变量配置完善

--- ui-ngx/src/app/modules/components/formly-config/configs/node-variables.config.ts
+++ ui-ngx/src/app/modules/components/formly-config/configs/node-variables.config.ts
@@ -1,5 +1,40 @@
 import { IEditorFormlyField } from '@src/app/shared/models/public-api';
 
+const variableType = [
+  {
+    label: "字符串",
+    value: "string"
+  },
+  {
+    label: "数值",
+    value: "number"
+  },
+  {
+    label: "布尔",
+    value: "boolean"
+  },
+  {
+    label: "日期",
+    value: "date"
+  },
+  {
+    label: "日期时间",
+    value: "datetime"
+  },
+  {
+    label: "对象",
+    value: "object"
+  },
+  {
+    label: "单值数组",
+    value: "singleArray"
+  },
+  {
+    label: "对象数组",
+    value: "objectArray"
+  }
+]
+
 export const variables_config: IEditorFormlyField[] = [
   {
     type: 'tabs',
@@ -23,150 +58,165 @@ export const variables_config: IEditorFormlyField[] = [
         className: 'hs-density--1',
         fieldGroup: [
           {
-            key: 'variableName',
-            type: 'input',
-            fieldId: 'input_key_2579558739748954',
-            props: {
-              label: '变量名',
-              typeName: '单行文本',
-              icon: 'text_fields',
-              row: 1,
-              placeholder: '请输入变量名',
-              disabled: false,
-              appearance: 'outline',
-            },
-            className: 'hs-density--5',
+            template: `<div>变量配置</div>`
           },
           {
-            key: 'getValueType',
-            type: 'radio',
-            fieldId: 'input_key_3723456456347834',
-            defaultValue: 'custom',
+            key: 'variableDefine',
+            type: 'array',
             props: {
-              label: '取值类型',
-              typeName: '单行文本',
-              icon: 'text_fields',
-              row: 1,
-              placeholder: '请选择取值类型',
-              disabled: false,
+              label: '变量',
               appearance: 'outline',
-              options: [
-                {
-                  label: "自定义",
-                  value: "custom"
-                },
-                {
-                  label: "系统",
-                  value: "system"
-                },
-                {
-                  label: "资产",
-                  value: "assets"
-                },
-                {
-                  label: "路由",
-                  value: "route"
-                }
-              ],
-            },
-            className: 'hs-density--5',
-          },
-          // 自定义变量，
-          {
-            key: 'variableType',
-            type: 'select',
-            fieldId: 'input_key_3723456456347834',
-            defaultValue: 'string',
-            props: {
-              label: '变量类型',
-              typeName: '单行文本',
-              icon: 'text_fields',
-              row: 1,
-              placeholder: '请选择变量类型',
-              disabled: false,
-              appearance: 'outline',
-              options: [
-                {
-                  label: "字符串",
-                  value: "string"
-                },
-                {
-                  label: "数值",
-                  value: "number"
-                },
-                {
-                  label: "布尔",
-                  value: "boolean"
-                },
-                {
-                  label: "日期",
-                  value: "date"
-                },
-                {
-                  label: "日期时间",
-                  value: "datetime"
-                },
-                {
-                  label: "对象",
-                  value: "object"
-                },
-                {
-                  label: "单值数组",
-                  value: "singleArray"
-                },
-                {
-                  label: "对象数组",
-                  value: "objectArrat"
-                }
-              ],
-            },
-            className: 'hs-density--5',
-          },
-          {
-            key: 'variableValue',
-            type: 'input',
-            fieldId: 'input_key_2579558739748954',
-            props: {
-              label: '变量值',
-              typeName: '单行文本',
-              icon: 'text_fields',
-              row: 1,
-              placeholder: '变量值',
-              disabled: false,
-              appearance: 'outline',
-            },
-            className: 'hs-density--5',
-          },
-          {
-            key: 'variableValue',
-            type: 'json-object',
-            fieldId: 'input_key_2579558739748954',
-            props: {
-              label: '变量值',
-              typeName: '单行文本',
-              icon: 'text_fields',
-              row: 1,
-              placeholder: '变量值',
-              disabled: false,
-              appearance: 'outline',
+              // 新增时默认添加的数据
+              defaultAddValue: {
+                variables: [],
+              },
+              direction: 'horizontal',
               styles: {
-                height: 200,
-                heightUnits: "px",
-                border: '1px solid var(--base-color-10)',
-                borderRadius: 8,
-                borderRadiusUnits: 'px',
-                overflow: "hidden"
+                display: 'flex',
+                flexDirection: 'column',
+                rowGap: 12,
+                rowGapUnits: 'px',
               },
-              title: "值"
+              itemStyles: {
+                padding: 16,
+                paddingUnits: 'px',
+                paddingTop: 12,
+                paddingTopUnits: 'px',
+                borderRadius: 6,
+                borderRadiusUnits: 'px',
+                boxShadow: '0px 0px 10px 2px var(--base-color-20)',
+                border: '1px solid var(--base-color-05)',
+              },
+              // hideDrag: true,
             },
             className: 'hs-density--5',
-            expressions: {
-              'props.title': (field: IEditorFormlyField) => {
-                const componentType = field.model?.variableType;
-                return componentType + "值";
-              },
-            },
-          },
+            fieldArray: {
+              fieldGroup: [
+                {
+                  type: 'grid',
+                  fieldId: 'grid_key_2491369384397947',
+                  props: {
+                    styles: {
+                      rowGap: 12,
+                      rowGapUnits: 'px',
+                    },
+                  },
+                  fieldGroup: [
+                    {
+                      key: 'variableName',
+                      type: 'input',
+                      fieldId: 'input_key_2579558739748954',
+                      props: {
+                        label: '变量名',
+                        typeName: '单行文本',
+                        icon: 'text_fields',
+                        placeholder: '请输入变量名',
+                        disabled: false,
+                        appearance: 'outline',
+                      },
+                      className: 'hs-density--5',
+                    },
+                    {
+                      key: 'getValueType',
+                      type: 'radio',
+                      fieldId: 'input_key_3723456456347834',
+                      defaultValue: 'custom',
+                      props: {
+                        label: '取值类型',
+                        typeName: '单行文本',
+                        icon: 'text_fields',
+                        placeholder: '请选择取值类型',
+                        disabled: false,
+                        appearance: 'outline',
+                        options: [
+                          {
+                            label: "自定义",
+                            value: "custom"
+                          },
+                          {
+                            label: "系统",
+                            value: "system"
+                          },
+                          {
+                            label: "资产",
+                            value: "assets"
+                          },
+                          {
+                            label: "路由",
+                            value: "route"
+                          }
+                        ],
+                      },
+                      className: 'hs-density--5',
+                    },
+                    // 自定义变量，
+                    {
+                      key: 'variableType',
+                      type: 'select',
+                      fieldId: 'input_key_3723456456347834',
+                      defaultValue: 'string',
+                      props: {
+                        label: '变量类型',
+                        typeName: '单行文本',
+                        icon: 'text_fields',
+                        placeholder: '请选择变量类型',
+                        disabled: false,
+                        appearance: 'outline',
+                        options: variableType,
+                      },
+                      className: 'hs-density--5',
+                    },
+                    {
+                      key: 'variableValue',
+                      type: 'input',
+                      fieldId: 'input_key_2579558739748954',
+                      props: {
+                        label: '变量值',
+                        typeName: '单行文本',
+                        icon: 'text_fields',
+                        placeholder: '变量值',
+                        disabled: false,
+                        appearance: 'outline',
+                      },
+                      className: 'hs-density--5',
+                      hideExpression: "!['string', 'number'].includes(model.variableType)"
+                    },
+                    {
+                      key: 'variableValue',
+                      type: 'json-object',
+                      fieldId: 'input_key_2579558739748954',
+                      props: {
+                        label: '变量值',
+                        typeName: '单行文本',
+                        icon: 'text_fields',
+                        placeholder: '变量值',
+                        disabled: false,
+                        appearance: 'outline',
+                        styles: {
+                          height: 200,
+                          heightUnits: "px",
+                          border: '1px solid var(--base-color-10)',
+                          borderRadius: 8,
+                          borderRadiusUnits: 'px',
+                          overflow: "hidden"
+                        },
+                        title: "值",
+                        type: "json"
+                      },
+                      className: 'hs-density--5',
+                      // expressions: {
+                      //   'props.title': (field: IEditorFormlyField) => {
+                      //     const componentType = field.model?.variableType;
+                      //     return componentType + "值";
+                      //   },
+                      // },
+                      hideExpression: "!['singleArray', 'objectArray', 'object'].includes(model.variableType)"
+                    },
+                  ]
+                }
+              ]
+            }
+          }
         ],
       },
       {
